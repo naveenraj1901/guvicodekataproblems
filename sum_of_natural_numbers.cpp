118d6315dc5c987001019e90a5f2a7f12d257c85
navenraj_raju <naveenrajvgp@gmail.com>
2017-09-02 12:47:05 +0530

    Update sum_of_natural_numbers.cpp

--- sum_of_natural_numbers.cpp
+++ sum_of_natural_numbers.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main() {
 	int val;
 	cin>>val;
-	cout<<((val*(val+1))/2);
+	if(val>0)
+	    cout<<((val*(val+1))/2);
+	else
+	    cout<<"non natural number";
 	return 0;
 }
