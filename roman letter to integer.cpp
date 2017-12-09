1325773d8a86fce93a11aa8cc0397077ebe45eff
navenraj_raju <naveenrajvgp@gmail.com>
2017-12-09 10:14:27 +0530

    Create roman letter to integer.cpp

---
+++ roman letter to integer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string roman_str;
+	int i,result=0;
+	cin>>roman_str;
+	for(i=0;roman_str[i]!='\0';i++)
+	{
+	    if(roman_str[i]=='I' || roman_str[i]=='i')
+	        result+=1;
+	    else if((roman_str[i]=='V' || roman_str[i]=='v') && (roman_str[i-1]=='I' || roman_str[i-1]=='i'))
+	        {result+=5;result-=2;}
+	    else if(roman_str[i]=='V' || roman_str[i]=='v')
+	        result+=5;
+	    else if((roman_str[i]=='X' || roman_str[i]=='x') && (roman_str[i-1]=='I' || roman_str[i-1]=='i'))
+	        {result+=10;result-=2;}
+	    else if((roman_str[i]=='X' || roman_str[i]=='x'))
+	        result+=10;
+	}
+	cout<<result;
+	return 0;
+}
