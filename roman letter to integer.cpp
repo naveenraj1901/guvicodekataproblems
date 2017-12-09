33b17303b8f5992981f316a355ec521c9102042b
navenraj_raju <naveenrajvgp@gmail.com>
2017-12-09 10:22:06 +0530

    Update roman letter to integer.cpp

--- roman letter to integer.cpp
+++ roman letter to integer.cpp
@@ -7,6 +7,8 @@ int main() {
 	cin>>roman_str;
 	for(i=0;roman_str[i]!='\0';i++)
 	{
+	    if(roman_str[i]=='I' || roman_str[i]=='i' || roman_str[i]=='V' || roman_str[i]=='v' ||roman_str[i]=='X' || roman_str[i]=='x')
+	    {
 	    if(roman_str[i]=='I' || roman_str[i]=='i')
 	        result+=1;
 	    else if((roman_str[i]=='V' || roman_str[i]=='v') && (roman_str[i-1]=='I' || roman_str[i-1]=='i'))
@@ -17,6 +19,11 @@ int main() {
 	        {result+=10;result-=2;}
 	    else if((roman_str[i]=='X' || roman_str[i]=='x'))
 	        result+=10;
+	    }
+	    else
+	    {
+	        result=0;cout<<result;return 0;
+	    }
 	}
 	cout<<result;
 	return 0;
